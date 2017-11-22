b0734611d5fc4de03fa0de2d35d1f9971842080c
Joe Brady <joe.brady@uk.ibm.com>
2017-11-22 12:02:06 +0000

    Updates

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const pg = require('pg')
+
+let client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
+
+client.connect((err) => {
+  if(err) {
+    console.error('DB connection unsuccessful', err.stack)
+  }
+  else {
+    console.log('DB connection successful')
+  }
+})
 
 // Route index page
 router.get('/', function (req, res) {
@@ -7,5 +22,12 @@ router.get('/', function (req, res) {
 })
 
 // Add your routes here - above the module.exports line
+
+router.post('/api/v1/joetest', function (req, res) {
+  console.log('Session: ', req.session)
+  let theirName = req.session.data['joe-test-name']
+  let theirLocation = req.session.data['joe-test-location']
+  console.log('Submitted!', `Their name is ${theirName}, and their location is ${theirLocation}.`)
+})
 
 module.exports = router
